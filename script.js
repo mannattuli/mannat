efe6d929d494c1998f8d412ae1a575e978733ca2
mannat <84506745+mannattuli@users.noreply.github.com>
2021-09-11 20:58:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,72 +1,28 @@
-/*Toggle projects hide show
-let buttonShow = document.querySelector(".hide");
-let toggleItem = document.querySelectorAll(".toggle-item");
+/*nav bar open*/
+let fa = document.querySelector(".fa-bars");
+let overlay = document.querySelector(".overlay");
 let hidden = document.querySelector(".hidden");
 
-for (let i = 0; i < toggleItem.length; i++) {
-  buttonShow.addEventListener("click", function () {
-    toggleItem[i].classList.toggle("hidden");
-    if (toggleItem[i].classList.contains("hidden")) {
-      buttonShow.text = "show";
-    } else {
-      buttonShow.text = "hide";
-    }
-  });
-}*/
-
-/*Dark mode*/
-$(".change").on("click", function () {
-  if ($("body").hasClass("dark")) {
-    $("body").removeClass("dark");
-    $(".change").removeClass("dark");
-    $(".toggle-item").removeClass("darkItem");
-    $(".toggle-itemBlog").removeClass("dark-item11");
-    $(".toggle-itemBlog").removeClass("darkItem");
-    $(".change").text("Light");
-  } else {
-    $("body").addClass("dark");
-    $(".change").addClass("dark");
-    $(".toggle-item").addClass("darkItem");
-    $(".toggle-itemBlog").addClass("darkItem");
-    $(".toggle-itemBlog").addClass("dark-item11");
-    $(".change").text("Dark");
-  }
+fa.addEventListener("click", function () {
+  overlay.classList.remove("hidden");
+  fa.classList.add("hidden");
 });
 
-/*Toggle blog hide*/
-let buttonShowBlog = document.querySelector(".hide-blog");
-let toggleItem1 = document.querySelectorAll(".toggle-itemBlog");
+/*nav bar close*/
+let times = document.querySelector(".fa-times");
 
-for (let i = 0; i < toggleItem1.length; i++) {
-  buttonShowBlog.addEventListener("click", function () {
-    toggleItem1[i].classList.toggle("hidden");
-    if (toggleItem1[i].classList.contains("hidden")) {
-      buttonShowBlog.text = "show";
-    } else {
-      buttonShowBlog.text = "hide";
-    }
-  });
-}
-
-/*Show All*/
-let dot = document.querySelector(".dot");
-let toggleItem = document.querySelectorAll(".toggle-item");
-
-dot.addEventListener("click", function () {
-  for (let i = 0; i < toggleItem.length - 1; i++) {
-    toggleItem[i].classList.toggle("hidden");
-  }
+times.addEventListener("click", function () {
+  overlay.classList.add("hidden");
+  fa.classList.remove("hidden");
 });
 
-/*Remove tweets when no space*/
-let aut = document.querySelector(".author-box");
+/*Change nav*/
+let navbla = document.querySelector(".bla-nav");
 
 if (window.matchMedia("(max-width: 700px)").matches) {
-  /*The viewport is less than, or equal to, 700 pixels wide*/
-  console.log("The viewport is less than, or equal to, 700 pixels wide");
-  aut.classList.add("hidden");
+  fa.classList.remove("hidden");
+  navbla.classList.add("hidden");
 } else {
-  /*The viewport is greater than 700 pixels wide*/
-  console.log("The viewport is greater than 700 pixels wide");
-  aut.classList.remove("hidden");
+  fa.classList.add("hidden");
+  navbla.classList.remove("hidden");
 }
